16d0094ede0c88237fb619da3de387c80a483bcc
jimis123 <74493496+jimis123@users.noreply.github.com>
2024-04-03 14:14:29 +0300

    Deployed website

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/bank_front_webstite/",
+    base: "/bank_modern_app/",
     plugins: [react()],
 })
